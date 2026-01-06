6291880555c9d9ec8ee2c24b9927cd865177ffd3
nikhilShakya7 <rsnikhil77@gmail.com>
2026-01-06 17:32:11 +0545

    optimized tests

--- portfolio test/tests/header.spec.ts
+++ portfolio test/tests/header.spec.ts
@@ -1,38 +1,38 @@
 import { test, expect } from "@playwright/test";
+test.describe("Header Test", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://www.shakyanikhil18.com.np/");
+    const header = page.locator("header");
+    await header.isVisible();
+  });
 
-test("Header is visible", async ({ page }) => {
-  await page.goto("https://www.shakyanikhil18.com.np/");
-  const header = page.locator("header");
-  await header.isVisible();
-});
+  test("Header has all text links", async ({ page }) => {
+    const header = page.locator("header");
+    const headerText = [
+      "Nikhil Shakya",
+      "Home",
+      "About",
+      "Projects",
+      "Contact",
+    ];
+    for (const text of headerText) {
+      await header.getByRole("link", { name: text }).isVisible();
+    }
+  });
 
-test("Header has all contents and links", async ({ page }) => {
-  await page.goto("https://www.shakyanikhil18.com.np/");
-  const header = page.locator("header");
-  await header.getByText("Nikhil Shakya").isVisible();
-  await header.getByRole("link", { name: "Home" }).isVisible();
-  await header.getByRole("link", { name: "About" }).isVisible();
-  await header.getByRole("link", { name: "Contact" }).isVisible();
-  await header.getByRole("link", { name: "Work" }).isVisible();
-});
-
-test("Links navigates to correct pages", async ({ page }) => {
-  await page.goto("https://www.shakyanikhil18.com.np/");
-  const header = page.locator("header");
-  await expect(header.getByRole("link", { name: "Home" })).toHaveAttribute(
-    "href",
-    "/"
-  );
-  await expect(header.getByRole("link", { name: "About" })).toHaveAttribute(
-    "href",
-    "/about"
-  );
-  await expect(header.getByRole("link", { name: "Work" })).toHaveAttribute(
-    "href",
-    "/project"
-  );
-  await expect(header.getByRole("link", { name: "Contact" })).toHaveAttribute(
-    "href",
-    "/contact"
-  );
+  test("Header links navigates to correct pages", async ({ page }) => {
+    const header = page.locator("header");
+    const headerLinks = [
+      // { name: "Nikhil Shakya", href: "/" },
+      { name: "Home", href: "/" },
+      { name: "Contact", href: "/contact" },
+      { name: "Work", href: "/project" },
+      { name: "About", href: "/about" },
+    ];
+    for (const links of headerLinks) {
+      await expect(
+        header.getByRole("link", { name: links.name })
+      ).toHaveAttribute("href", links.href);
+    }
+  });
 });
