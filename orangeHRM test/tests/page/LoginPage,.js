289ab89eaf3c80b36a77ce50b45b96507e35bdc9
nikhilShakya7 <rsnikhil77@gmail.com>
2026-01-07 16:05:30 +0545

    Refactored login.spec.js to use LoginPage class

---
+++ orangeHRM test/tests/page/LoginPage,.js
@@ -0,0 +1,21 @@
+export class LoginPage {
+  constructor(page) {
+    this.page = page;
+
+    this.usernameInput = page.locator("#username");
+    this.passwordInput = page.locator("#password");
+    this.loginButton = page.locator("#loginBtn");
+
+    this.errorMessage = page.locator("#error-msg");
+  }
+
+  async goto() {
+    await this.page.goto("/login");
+  }
+
+  async login(username, password) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
