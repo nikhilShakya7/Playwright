c1ac276ffe31eb345aa21495b4240cd569861848
nikhilShakya7 <rsnikhil77@gmail.com>
2026-01-13 16:14:10 +0545

    `Removed test files and configuration for Playwright tests`

---
+++ orangeHRM test/tests/pages/Dashboard.ts
@@ -0,0 +1,14 @@
+import { Page } from "@playwright/test";
+import BasePage from "./BasePage";
+
+export class Dashboard extends BasePage {
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async openDashboard() {
+    await this.goto(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    );
+  }
+}
---
+++ orangeHRM test/tests/pages/LoginPage.ts
@@ -0,0 +1,34 @@
+import { expect, Locator, Page } from "@playwright/test";
+import BasePage from "./BasePage";
+
+export class LoginPage extends BasePage {
+  readonly username: Locator;
+  readonly userpassword: Locator;
+  readonly loginButton: Locator;
+  readonly errorMessage: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.username = page.getByPlaceholder("Username");
+    this.userpassword = page.getByPlaceholder("Password");
+    this.loginButton = page.getByRole("button", { name: "Login" });
+    this.errorMessage = page.getByRole("alert");
+  }
+
+  async open() {
+    await this.goto(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+  }
+
+  async login(name: string, password: string) {
+    await this.username.fill(name);
+    await this.userpassword.fill(password);
+    await this.loginButton.click();
+  }
+
+  async expError(message: string) {
+    await expect(this.errorMessage).toBeVisible;
+    await expect(this.errorMessage).toHaveText(message);
+  }
+}
